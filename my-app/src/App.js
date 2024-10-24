757d23527a1c56c6bfb2fdc274cfd7689f015c37
Bisma <bismaazam60kk@gmail.com>
2024-10-24 17:03:02 +0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calc from './Components/Calc';
+
+function App() {
+  return (
+   <>
+       <div>
+        <Calc />
+       </div>
+   </>
+  );
+}
+
+export default App;
